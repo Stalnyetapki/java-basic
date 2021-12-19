3251e548c1b5074afee731915794d0eddffa73a0
opachki <a.tupikov.miel@gmail.com>
2021-12-19 12:00:28 +0100

    first changes in master branch

--- src/basic/Main.java
+++ src/basic/Main.java
@@ -15,7 +15,7 @@ public class Main {
         List<String> strings  = Arrays.asList("два", "один", "три");
 
         Map<String, Integer> digestMap = new HashMap<>();
-        digestMap.put("один", 1);
+        digestMap.put("one", 1);
         digestMap.put("два", 2);
         digestMap.put("три", 3);
 
